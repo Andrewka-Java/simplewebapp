ea9d528fc176b525104851420d59ccf8d2839a0e
Andrewka-Java <alze.andrey.1997@mail.ru>
2021-08-22 16:18:16 +0300

    BUILD: return previous state for AuthResponse

--- backend/src/main/java/com/mastery/java/task/security/AuthResponse.java
+++ backend/src/main/java/com/mastery/java/task/security/AuthResponse.java
@@ -5,9 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
 
-    private final String jwtToken;
+    private String jwtToken;
 
 }
